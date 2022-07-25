8ae4d3d7a222847dad75a7acf663f955a9c746ef
guislakkkkkkk <107629042+guislakkkkkkk@users.noreply.github.com>
2022-07-25 01:52:57 -0300

    Delete assets/preload/data/Atrocity directory

--- assets/preload/data/Atrocity/script.lua
+++
@@ -1,8 +0,0 @@
-function onCreate()
-
-    setPropertyFromClass('GameOverSubstate', 'characterName', 'bf');
-    setPropertyFromClass('GameOverSubstate', 'deathSoundName', 'fnf_loss_sfx');
-    setPropertyFromClass('GameOverSubstate', 'loopSoundName', 'gameOver');
-    setPropertyFromClass('GameOverSubstate', 'endSoundName', 'gameOverEnd');
- 
-end 
